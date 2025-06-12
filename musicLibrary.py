51efb917c7ebe1b9a732a91f41962e859d7a2cfa
Ipshita-gupt <ipshitagupta1602@gmail.com>
2025-06-12 19:41:52 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "Faded" : "https://youtu.be/60ItHLz5WEA?si=QHLaMp61VDCGHDYd",
+    "On My Way" : "https://youtu.be/dhYOPzcsbGM?si=UMo9lQLTcQTbdi8F",
+    "Lily" : "https://youtu.be/ox4tmEV6-QU?si=G2PoMNeN7seoThPG",
+    "Aaakhein Khuli" : "https://youtu.be/eM8Mjuq4MwQ?si=uafvHkU7UMxouIYq",
+    "Tennu le" : "https://youtu.be/U_Eqj9pGbcM?si=NXjMqTtVsicR25lb",
+    "Dil nu" : "https://youtu.be/p2EdDiiVHh4?si=1LL7dfhQLtamOsol",
+    "Khaab" : "https://youtu.be/2eliQ_KR8yA?si=Dt9jh2prPMD9xpcw"
+}
