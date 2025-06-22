bb9ff993274df3fa4aafa082e06982b14cd9f51a
Ipshita Gupta <ipshitagupta1602@gmail.com>
2025-06-22 22:26:25 +0530

    main.py

--- main.py
+++ main.py
@@ -59,8 +59,6 @@ def processCommand(c):
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
     while True:
-        # Listen for the wake word "Jarvis"
-        # obtain audio from the microphone
         r = sr.Recognizer()
          
         print("recognizing...")
